326a87ba5a3679601a23f0354f8569920f8ab9de
geunhokinn <gghh1220@gmail.com>
2024-05-27 20:32:48 +0900

    feat: 값 타입 컬렉션

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,11 +17,45 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Address address1 = new Address("city", "street", "10000");
-            Address address2 = new Address("city", "street", "10000");
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(new Address("homeCity", "street", "10000")); // 임베디드 타입
 
-            System.out.println("(address1 == address2) = " + (address1 == address2));
-            System.out.println("address1.equals(address2) = " + (address1.equals(address2)));
+            member.getFavoriteFoods().add("치킨"); // 값 타입 컬렉션
+            member.getFavoriteFoods().add("족발"); // 값 타입 컬렉션
+            member.getFavoriteFoods().add("피자"); // 값 타입 컬렉션
+
+            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
+            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("==============");
+            Member findMember = em.find(Member.class, member.getId());
+
+//            List<Address> addressHistory = findMember.getAddressHistory(); // 값 타입 컬렉션은 지연 로딩이다.
+//            for (Address address : addressHistory) {
+//                System.out.println("address.getCity() = " + address.getCity());
+//            }
+
+            // homeCity -> newCity // 값 타입
+//            findMember.getHomeAddress().setCity("newCity");
+            // 지금은 private 으로 막아서 안되는데 허용하면 모든 인스턴스가 바뀌므로 직접 바꾸면 안 된다.
+            // 새로 생성해서 넣어야 한다. 즉 address 인스턴스를 생성해서 완전히 교체를 해야 한다.
+//            Address oldAddress = findMember.getHomeAddress();
+//            findMember.setHomeAddress(new Address("newCity", oldAddress.getStreet(), oldAddress.getZipcode()));
+
+            // 치킨 -> 한식 // 값 타입 컬렉션 set
+            // String 자체가 값 타입이므로 업데이트를 할 수 없으므로 제거하고 추가해야 한다.
+//            findMember.getFavoriteFoods().remove("치킨");
+//            findMember.getFavoriteFoods().add("한식");
+
+            // old1 -> newCity1 // 값 타입 컬렉션 list
+//            System.out.println("==============");
+//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
+//            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,7 +3,9 @@ package hellojpa;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 //public class Member extends BaseEntity {
@@ -17,13 +19,38 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
-    // 기간 Period
-    @Embedded
-    private Period workPeriod;
+//    // 기간 Period
+//    @Embedded
+//    private Period workPeriod;
 
-    // 집 주소 주소
+    // 집 주소 주소(값 타입)
     @Embedded
     private Address homeAddress;
+
+    @ElementCollection // 값 타입 컬렉션
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name = "FOOD_NAME") // 내가 정의한 것이 아니고 컬럼명이 하나이다. 예외적으로 허용해준다.
+    private Set<String> favoriteFoods = new HashSet<>();
+
+//    @ElementCollection // 값 타입 컬렉션
+//    @CollectionTable(name = "ADDRESS", joinColumns =
+//        @JoinColumn(name = "MEMBER_ID")
+//    )
+//    private List<Address> addressHistory = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
+    }
 
     // 회사 주소 주소
 //    @Embedded
@@ -34,13 +61,13 @@ public class Member {
 //    })
 //    private Address workAddress;
 
-    public Period getWorkPeriod() {
-        return workPeriod;
-    }
-
-    public void setWorkPeriod(Period workPeriod) {
-        this.workPeriod = workPeriod;
-    }
+//    public Period getWorkPeriod() {
+//        return workPeriod;
+//    }
+//
+//    public void setWorkPeriod(Period workPeriod) {
+//        this.workPeriod = workPeriod;
+//    }
 
     public Address getHomeAddress() {
         return homeAddress;
@@ -48,6 +75,14 @@ public class Member {
 
     public void setHomeAddress(Address homeAddress) {
         this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
     }
 
     //    @ManyToOne(fetch = FetchType.LAZY) // team 을 프록시 객체로 조회함
